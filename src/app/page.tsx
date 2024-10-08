96ee272b2f12f0699e48956a10b41761d5bbac8f
fakepixels <tinaxhe@gmail.com>
2024-10-08 14:25:31 -0400

    mobile responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,19 +12,20 @@ export default function Page() {
   const { address } = useAccount();
 
   return (
-    <div className="flex h-full w-96 max-w-full flex-col px-1 md:w-[1008px]">
+    <div className="flex h-full w-full max-w-full flex-col overflow-x-hidden px-1 md:w-[1008px]">
       <section className="mt-6 mb-6 flex w-full flex-col md:flex-row">
-        <div className="flex w-full flex-row items-center justify-between gap-2 md:gap-0">
+        <div className='flex w-full flex-row items-center justify-between gap-2 md:justify-end md:gap-0'>
           <a
             href={ONCHAINKIT_LINK}
             title="onchainkit"
             target="_blank"
             rel="noreferrer"
+            className="hidden md:block" // Hide on mobile
           >
             <OnchainkitSvg />
           </a>
 
-          <div className="flex items-center gap-3">
+          <div className='flex w-full items-center justify-end gap-3'>
             <SignupButton />
             {!address && <LoginButton />}
             <FundComponent />
@@ -32,14 +33,18 @@ export default function Page() {
         </div>
       </section>
       <section 
-        className='flex flex-col items-center justify-center'
+        className='flex w-full flex-col items-center justify-center'
         style={{ height: '760px' }}
       >
       
       {/* NFT display section */}
       <section className="SkuSection flex w-full flex-col items-center justify-center rounded-xl px-2 py-4 md:grow">
-        <div className='flex flex-col items-center justify-center py-4'>
-          <img src="./image.png" alt="merch" className="w-full max-w-lg rounded-xl border-2 border-gray-100" />
+        <div className='flex w-full flex-col items-center justify-center py-4'>
+          <img 
+            src="./image.png" 
+            alt="merch" 
+            className='w-80 max-w-xs rounded-xl border-2 border-gray-100 md:w-full md:max-w-lg' 
+          /> 
           {address && <TransactionComponent address={address} />}
         </div>
       </section>
